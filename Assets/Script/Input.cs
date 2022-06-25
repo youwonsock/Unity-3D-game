b947f91132577bd72f937fa5330fe6f3c34ca98f
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-06-25 23:58:51 +0900

    Add Floor, Wall, player prefab

---
+++ Assets/Script/Input.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input : MonoBehaviour
+{
+    float hAxis;
+    float vAxis;
+
+    Vector3 moveVec;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
