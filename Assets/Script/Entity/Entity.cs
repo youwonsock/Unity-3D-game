1ec3a675503801685d7cb99c4f070437d9327bfe
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-06-28 02:35:03 +0900

    ADD Entity
    Entity is Base Class of Player or monster

--- Assets/Script/Entity/Entity.cs
+++ Assets/Script/Entity/Entity.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * @brief 
+ * @details 
+ * 
+ * @author yws
+ * @data last change 2022/06/28
+ */
 public class Entity : MonoBehaviour
 {
     [SerializeField] private float Health;
