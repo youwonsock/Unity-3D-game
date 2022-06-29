334ffe9e99477a33a80f0abc638a94be284e3244
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-06-29 23:43:45 +0900

    Update Entity.cs
    add comment

--- Assets/Script/Entity/Entity.cs
+++ Assets/Script/Entity/Entity.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /**
- * @brief 
- * @details 
+ * @brief class 게임내 플레이어, 몬스터등의 생명체의 베이스 클래스
+ * @details 게임내 생명체들의 최상위 클래스입니다.
  * 
  * @author yws
  * @data last change 2022/06/28
