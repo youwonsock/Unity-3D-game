b52d10731cf06c3a6c5603485b897f8d08e2dc89
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-07-03 22:28:36 +0900

    Add Hammer.cs

---
+++ Assets/Script/Item/Weapon/Hammer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief Class 
+ * @details 
+ * 
+ * @author yws
+ * @date last change 2022/07/03
+ */
+public class Hammer : Item
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
