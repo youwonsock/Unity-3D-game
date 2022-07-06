44a15664d9572199ebbe25f8b9c3601994ca8ae1
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-07-07 02:30:36 +0900

    Update PlayerWeapon.cs
    Add Comment
    change Swap()
              GetWeapon()

--- Assets/Script/Entity/Player/PlayerWeapon.cs
+++ Assets/Script/Entity/Player/PlayerWeapon.cs
@@ -7,7 +7,7 @@ using UnityEngine;
  * @details Player가 사용하는 무기들을 배열로 가지
  * 
  * @author yws
- * @date last change 2022/06/28
+ * @date last change 2022/07/07
  */
 public class PlayerWeapon : MonoBehaviour
 {
@@ -16,13 +16,15 @@ public class PlayerWeapon : MonoBehaviour
     [SerializeField] private Weapon[] weapons;
     [SerializeField] private bool[] hasWeapon;
 
-    private int currentWeaponIdx;
+    private int currentWeaponIdx = 0;
 
     #endregion
 
 
 
     #region Property
+
+    public int CurrentWeaponIdx() { return currentWeaponIdx; }
 
     #endregion
 
@@ -32,25 +34,57 @@ public class PlayerWeapon : MonoBehaviour
 
     /**
      * @brief 무기 변경 메서드(PlayerWeapon)
-     * @details Item의 OnTriggerExit()에서 실행시킬 동작을 override로 정의하는 메서드입니다.
+     * @details Player에서 호출되어 무기를 변경하는 메서드
+     * hasWeapon[swapIdx]가 false이거나 바꾸려는 무기의 idx가 현재 무기와 같다면 하여 메서드를 종료합니다.
+     * return bool
      * 
      * @author yws
      * @data last change 2022/07/07
      */
-    public void Swap(int swapIdx)
+    public bool Swap(int swapIdx)
     {
-        switch(swapIdx)
+        if (!hasWeapon[swapIdx] || swapIdx == currentWeaponIdx)
+            return false;
+
+        weapons[currentWeaponIdx].gameObject.SetActive(false);
+        currentWeaponIdx = swapIdx;
+
+        weapons[currentWeaponIdx].gameObject.SetActive(true);
+
+        return true;
+    }
+
+    /**
+     * @brief 무기 획득 메서드
+     * @details Item에서 호출되어 획득한 무기를 활성화 시키는 메서드
+     * @return bool
+     * 
+     * @author yws
+     * @data last change 2022/07/07
+     */
+    public bool GetWeapon(int itemValue, bool interactionInput)
+    {
+        if (!interactionInput)
+            return false;
+
+        for(int i = 0; i < weapons.Length; i++)
         {
-            case 1:
-                // 
-                break;
-            case 2:
-                //
-                break;
-            case 3:
-                //
-                break;
+            if (weapons[i].Value() == itemValue && !hasWeapon[i])
+            {
+                if (hasWeapon[i])
+                    continue;
+
+                weapons[currentWeaponIdx].gameObject.SetActive(false);
+                weapons[i].gameObject.SetActive(true);
+
+                hasWeapon[i] = true;
+                currentWeaponIdx = i;
+
+                return true;
+            }
         }
+
+        return false;
     }
 
     #endregion
