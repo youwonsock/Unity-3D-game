2ae21f27c6d1b339e4d195b35276f5fbf8ff19ac
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-07-15 23:26:11 +0900

    Add Bullet.cs

---
+++ Assets/Script/Item/Weapon/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief class 총알의 기능을 구현한 클레스
+ * @details 
+ * 
+ * @author yws
+ * @date last change 2022/07/15
+ */
+public class Bullet : MonoBehaviour
+{
+
+}
