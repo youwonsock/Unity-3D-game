299f02049acee44aac0fe546ce22e8693ac5a91f
youwonsock <46276141+youwonsock@users.noreply.github.com>
2022-08-11 01:43:09 +0900

    Add UiManager.cs

---
+++ Assets/Script/Utility/Manager/UiManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief class UI 설정을 위한 Manager 클래스
+ * 
+ * @author yws
+ * @date last change 2022/08/11
+ */
+public class UiManager : MonoBehaviour
+{
+    #region Fields
+
+
+    #endregion
+
+
+
+    #region Property
+
+
+    #endregion
+
+
+
+    #region Funtion
+
+    //--------------------------public--------------------------------------
+
+
+
+    //--------------------------private--------------------------------------
+
+    #endregion
+
+
+
+    #region Unity Event
+
+
+    #endregion
+
+
+}
